43dd4ea69e91d21e8ef2b164644c9a1018e092cd
veer2041 <rveeragoudar@gmail.com>
2017-06-25 17:42:16 +0530

    Initial commit

---
+++ app/src/main/java/com/lovish/audioload/MainActivity.java
@@ -0,0 +1,422 @@
+package com.lovish.audioload;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Random;
+
+import static android.Manifest.permission.RECORD_AUDIO;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private static final int PICK_FILE_REQUEST = 1;
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private String selectedFilePath;
+    private String SERVER_URL = "http://192.168.1.8/audio/uploadtoserver.php";
+    ImageView ivAttachment;
+    Button bUpload;
+    TextView tvFileName;
+    ProgressDialog dialog;
+    Button buttonStart, buttonStop, buttonPlayLastRecordAudio,
+            buttonStopPlayingRecording ;
+    String AudioSavePathInDevice = null;
+    MediaRecorder mediaRecorder ;
+    Random random ;
+    String RandomAudioFileName = "ABCDEFGHIJKLMNOP";
+    public static final int RequestPermissionCode = 1;
+    MediaPlayer mediaPlayer ;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ivAttachment = (ImageView) findViewById(R.id.ivAttachment);
+        bUpload = (Button) findViewById(R.id.b_upload);
+        tvFileName = (TextView) findViewById(R.id.tv_file_name);
+        ivAttachment.setOnClickListener(this);
+        bUpload.setOnClickListener(this);
+
+
+        buttonStart = (Button) findViewById(R.id.record);
+        buttonStop = (Button) findViewById(R.id.stop);
+        buttonPlayLastRecordAudio = (Button) findViewById(R.id.play);
+        buttonStopPlayingRecording = (Button)findViewById(R.id.stop_play);
+
+        buttonStop.setEnabled(false);
+        buttonPlayLastRecordAudio.setEnabled(false);
+        buttonStopPlayingRecording.setEnabled(false);
+
+        random = new Random();
+
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(checkPermission()) {
+
+                    AudioSavePathInDevice = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC).getAbsolutePath() + "/" +
+                                    CreateRandomAudioFileName(5) + "AudioRecording.wav";
+
+                    MediaRecorderReady();
+
+                    try {
+                        mediaRecorder.prepare();
+                        mediaRecorder.start();
+                    } catch (IllegalStateException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+
+                    buttonStart.setEnabled(false);
+                    buttonStop.setEnabled(true);
+
+                    Toast.makeText(MainActivity.this, "Recording started",
+                            Toast.LENGTH_LONG).show();
+                } else {
+                    requestPermission();
+                }
+
+            }
+        });
+
+        buttonStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaRecorder.stop();
+                buttonStop.setEnabled(false);
+                buttonPlayLastRecordAudio.setEnabled(true);
+                buttonStart.setEnabled(true);
+                buttonStopPlayingRecording.setEnabled(false);
+                Toast.makeText(MainActivity.this, "Recording Completed",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
+        buttonPlayLastRecordAudio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) throws IllegalArgumentException,
+                    SecurityException, IllegalStateException {
+
+                buttonStop.setEnabled(false);
+                buttonStart.setEnabled(false);
+                buttonStopPlayingRecording.setEnabled(true);
+
+                mediaPlayer = new MediaPlayer();
+                try {
+                    mediaPlayer.setDataSource(AudioSavePathInDevice);
+                    tvFileName.setText(AudioSavePathInDevice);
+                    mediaPlayer.prepare();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                mediaPlayer.start();
+                Toast.makeText(MainActivity.this, "Recording Playing",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
+        buttonStopPlayingRecording.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                buttonStop.setEnabled(false);
+                buttonStart.setEnabled(true);
+                buttonStopPlayingRecording.setEnabled(false);
+                buttonPlayLastRecordAudio.setEnabled(true);
+
+                if(mediaPlayer != null){
+                    mediaPlayer.stop();
+                    mediaPlayer.release();
+                    MediaRecorderReady();
+                }
+            }
+        });
+
+
+
+
+
+
+    }
+    public boolean checkPermission() {
+        int result = ContextCompat.checkSelfPermission(getApplicationContext(),
+                WRITE_EXTERNAL_STORAGE);
+        int result1 = ContextCompat.checkSelfPermission(getApplicationContext(),
+                RECORD_AUDIO);
+        return result == PackageManager.PERMISSION_GRANTED &&
+                result1 == PackageManager.PERMISSION_GRANTED;
+    }
+
+    public void MediaRecorderReady(){
+        mediaRecorder=new MediaRecorder();
+        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mediaRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
+        mediaRecorder.setOutputFile(AudioSavePathInDevice);
+    }
+
+    public String CreateRandomAudioFileName(int string){
+        StringBuilder stringBuilder = new StringBuilder( string );
+        int i = 0 ;
+        while(i < string ) {
+            stringBuilder.append(RandomAudioFileName.
+                    charAt(random.nextInt(RandomAudioFileName.length())));
+
+            i++ ;
+        }
+        return stringBuilder.toString();
+    }
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(MainActivity.this, new
+                String[]{WRITE_EXTERNAL_STORAGE, RECORD_AUDIO}, RequestPermissionCode);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case RequestPermissionCode:
+                if (grantResults.length> 0) {
+                    boolean StoragePermission = grantResults[0] ==
+                            PackageManager.PERMISSION_GRANTED;
+                    boolean RecordPermission = grantResults[1] ==
+                            PackageManager.PERMISSION_GRANTED;
+
+                    if (StoragePermission && RecordPermission) {
+                        Toast.makeText(MainActivity.this, "Permission Granted",
+                                Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(MainActivity.this,"Permission Denied",Toast.LENGTH_LONG).show();
+                    }
+                }
+                break;
+        }
+    }
+
+
+
+
+
+    @Override
+    public void onClick(View v) {
+        if(v== ivAttachment){
+
+            //on attachment icon click
+            showFileChooser();
+        }
+        if(v== bUpload){
+
+            //on upload button Click
+            if(selectedFilePath != null){
+                dialog = ProgressDialog.show(MainActivity.this,"","Uploading File...",true);
+
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        //creating new thread to handle Http Operations
+                        uploadFile(selectedFilePath);
+                    }
+                }).start();
+            }else{
+                Toast.makeText(MainActivity.this,"Please choose a File First",Toast.LENGTH_SHORT).show();
+            }
+
+        }
+    }
+
+    private void showFileChooser() {
+        Intent intent = new Intent();
+         intent.setType("*/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+    //   startActivityForResult(Intent.createChooser(intent,"Choose File to Upload.."),PICK_FILE_REQUEST);
+        startActivityForResult(Intent.createChooser(intent,"Choose File to Upload.."),PICK_FILE_REQUEST);
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == Activity.RESULT_OK){
+            if(requestCode == PICK_FILE_REQUEST){
+                if(data == null){
+                    //no data present
+                    return;
+                }
+
+
+                Uri selectedFileUri = data.getData();
+                Log.d("mainActivity"," selected file from folder " + selectedFileUri);
+                selectedFilePath = FilePath.getPath(this,selectedFileUri);
+                Log.i(TAG,"Selected File Path:" + selectedFilePath);
+
+                if(selectedFilePath != null && !selectedFilePath.equals("")){
+                    tvFileName.setText(selectedFilePath);
+                }else{
+                    Toast.makeText(this,"Cannot upload file to server",Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    //android upload file to server
+    public int uploadFile(final String selectedFilePath){
+        int serverResponseCode = 0;
+        HttpURLConnection connection;
+        DataOutputStream dataOutputStream;
+        String lineEnd = "\r\n";
+        String twoHyphens = "--";
+        String boundary = "*****";
+
+
+        int bytesRead,bytesAvailable,bufferSize;
+        byte[] buffer;
+        int maxBufferSize = 1 * 1024 * 1024 * 1024;
+        File selectedFile = new File(selectedFilePath);
+
+        Log.d("MainActivity", "selected file path " + selectedFile);
+
+        String[] parts = selectedFilePath.split("/");
+        final String fileName = parts[parts.length-1];
+        Log.d("MainActivity", "File name " + fileName);
+
+        if (!selectedFile.isFile()){
+            dialog.dismiss();
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    tvFileName.setText("Source File Doesn't Exist: " + selectedFilePath);
+                }
+            });
+            return 0;
+        }else{
+            try{
+                FileInputStream fileInputStream = new FileInputStream(selectedFile);
+
+                Log.d("MainActivity", "file inputstrem " + fileInputStream);
+                URL url = new URL(SERVER_URL);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.setDoInput(true);//Allow Inputs
+                connection.setDoOutput(true);//Allow Outputs
+                connection.setUseCaches(false);//Don't use a cached Copy
+                connection.setRequestMethod("POST");
+                connection.setRequestProperty("Connection", "Keep-Alive");
+                connection.setRequestProperty("ENCTYPE", "multipart/form-data");
+                connection.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
+                connection.setRequestProperty("uploaded_file",selectedFilePath);
+
+                //creating new dataoutputstream
+                dataOutputStream = new DataOutputStream(connection.getOutputStream());
+
+                //writing bytes to data outputstream
+                dataOutputStream.writeBytes(twoHyphens + boundary + lineEnd);
+                dataOutputStream.writeBytes("Content-Disposition: form-data; name=\"uploaded_file\";filename=\""
+                        + selectedFilePath + "\"" + lineEnd);
+
+                dataOutputStream.writeBytes(lineEnd);
+
+                //returns no. of bytes present in fileInputStream
+                bytesAvailable = fileInputStream.available();
+                //selecting the buffer size as minimum of available bytes or 1 MB
+                bufferSize = Math.min(bytesAvailable,maxBufferSize);
+                //setting the buffer as byte array of size of bufferSize
+                buffer = new byte[bufferSize];
+
+                //reads bytes from FileInputStream(from 0th index of buffer to buffersize)
+                bytesRead = fileInputStream.read(buffer,0,bufferSize);
+
+                Log.d("MainActivity", "bytesread " + bytesRead);
+
+                //loop repeats till bytesRead = -1, i.e., no bytes are left to read
+                while (bytesRead > 0){
+                    //write the bytes read from inputstream
+                    dataOutputStream.write(buffer,0,bufferSize);
+                    bytesAvailable = fileInputStream.available();
+                    Log.d("MainActivity", "byteavailable " + bytesAvailable);
+                    bufferSize = Math.min(bytesAvailable,maxBufferSize);
+                    Log.d("MainActivity", "buffersize" + bufferSize);
+                    bytesRead = fileInputStream.read(buffer,0,bufferSize);
+                    Log.d("MainActivity", "bytesread " + bytesRead);
+                }
+
+                dataOutputStream.writeBytes(lineEnd);
+                dataOutputStream.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+
+                serverResponseCode = connection.getResponseCode();
+                String serverResponseMessage = connection.getResponseMessage();
+
+                Log.i(TAG, "Server Response is: " + serverResponseMessage + ": " + serverResponseCode);
+
+                //response code of 200 indicates the server status OK
+                if(serverResponseCode == 200){
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            tvFileName.setText("File Upload completed. You can see the uploaded file here:" + "http://192.168.1.8/audio/uploads/"+ fileName);
+                        }
+                    });
+                }
+
+                //closing the input and output streams
+                fileInputStream.close();
+                dataOutputStream.flush();
+                dataOutputStream.close();
+
+
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(MainActivity.this,"File Not Found",Toast.LENGTH_SHORT).show();
+                    }
+                });
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+                Toast.makeText(MainActivity.this, "URL error!", Toast.LENGTH_SHORT).show();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                Toast.makeText(MainActivity.this, "Cannot Read/Write File!", Toast.LENGTH_SHORT).show();
+            }
+            dialog.dismiss();
+            return serverResponseCode;
+        }
+
+    }
+}
